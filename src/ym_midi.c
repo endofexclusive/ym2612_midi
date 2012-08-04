f8e045360150dcc7cd1f45e32a441c60b54a87cb
Martin Åberg <martin@fripost.org>
2012-08-04 03:59:57 +0200

    Implemented CC multiplexer.

--- src/ym_midi.c
+++ src/ym_midi.c
@@ -16,6 +16,107 @@ this program (COPYING).  If not, see <http://www.gnu.org/licenses/>. */
 #include "ym_midi.h"
 #include "ym.h"
 
+#define CC_SET_ALGORITHM 3
+#define CC_SET_OP1_SELF_FEEDBACK 9
+#define CC_SET_PAN 10
+#define CC_SET_LFO_AM_SENSITIVITY 14
+#define CC_SET_LFO_FM_SENSITIVITY 15
+#define CC_SET_LFO_FREQUENCY 16
+#define CC_SET_CHANNEL36_MODE 17
+#define CC_SET_AMPLITUDE 20
+#define CC_SET_ATTACK_RATE 24
+#define CC_SET_D1R 28
+#define CC_SET_D1L 52
+#define CC_SET_D2R 56
+#define CC_SET_RR 60
+#define CC_SET_AM 104
+#define CC_SET_RATE_SCALING 108
+#define CC_SET_MULTIPLIER 112
+#define CC_SET_DETUNE 116
+
+static uint8_t scale(uint8_t n)
+{
+    return 128/n;
+}
+
+/* Implementation of "MIDI Implementation Chart v 2.0, Control
+Number Information". A lookup-table based approach has been
+considered but the focus for now is on saving data memory
+(not code memory). The function has no channel guard. */
+static void ym_midi_put_cc(Ym_midi *self, Midi_message *m)
+{
+    const uint8_t channel = m->status & 0x0f;
+    const uint8_t controller = m->data1;
+    const uint8_t value = m->data2;
+    Ym_driver *driver = self->driver;
+
+    switch (controller) {
+    case CC_SET_ALGORITHM:
+        ym_set_algorithm(driver, channel, value/scale(8));
+        break;
+    case CC_SET_OP1_SELF_FEEDBACK:
+        ym_set_op1_self_feedback(driver, channel, value/scale(8));
+        break;
+    case CC_SET_PAN:
+        ym_set_pan(driver, channel, value/scale(3));
+        break;
+    case CC_SET_LFO_AM_SENSITIVITY:
+    case CC_SET_LFO_FM_SENSITIVITY:
+    case CC_SET_LFO_FREQUENCY:
+    case CC_SET_CHANNEL36_MODE:
+        break;
+    default:
+        {
+            /* These assignments depend on the layout of CC the
+            numbers. */
+            uint8_t operator = controller / 4;
+            uint8_t function = controller - operator;
+
+            switch (function) {
+            case 20:
+                ym_set_amplitude(driver, channel, operator,
+                  value/scale(128));
+                break;
+            case 24:
+                ym_set_attack_rate(driver, channel, operator,
+                  value/scale(32));
+                break;
+            case 28:
+                ym_set_d1r(driver, channel, operator,
+                  value/scale(32));
+                break;
+            case 52:
+                ym_set_d1l(driver, channel, operator,
+                  value/scale(16));
+                break;
+            case 56:
+                ym_set_d2r(driver, channel, operator,
+                  value/scale(32));
+                break;
+            case 60:
+                ym_set_rr(driver, channel, operator,
+                  value/scale(16));
+                break;
+            case 104:
+                ym_set_am(driver, channel, operator, 0 != value);
+                break;
+            case 108:
+                ym_set_rate_scaling(driver, channel, operator,
+                  value/scale(4));
+                break;
+            case 112:
+                ym_set_multiplier(driver, channel, operator,
+                  value/scale(16));
+                break;
+            case 116:
+                ym_set_detune(driver, channel, operator,
+                  value/scale(8));
+                break;
+            }
+        }
+    }
+}
+
 static void ym_midi_put(Ym_midi *self, Midi_message *m)
 {
     uint8_t channel;
@@ -36,6 +137,7 @@ static void ym_midi_put(Ym_midi *self, Midi_message *m)
     } else if (midi_message_is_control(m)) {
         dprint("Control change, channel %x, #%x, value %x\n",
           m->status & 0x0f, m->data1, m->data2);
+        ym_midi_put_cc(self, m);
     } else if (midi_message_is_mode(m)) {
         dprint("Channel mode, channel %x, #%x, value %x\n",
           m->status & 0x0f, m->data1, m->data2);
